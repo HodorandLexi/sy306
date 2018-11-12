deb69716a895e38a1ba67bf417597801b397244d
HodorandLexi <jmj89421844@163.com>
2018-11-12 16:51:59 +0800

    no message

--- scripts/test_contact.py
+++ scripts/test_contact.py
@@ -2,15 +2,15 @@ import pytest
 
 
 class TestContact:
-	
+
 	def test_a(self):
 		assert 1
-        
-    def test_b(self):
-        assert 1
-        
-    def test_c(self):
-        assert 1
 
-    def test_d(self):
-        assert 0
+	def test_b(self):
+		assert 1
+
+	def test_c(self):
+		assert 1
+
+	def test_d(self):
+		assert 0
