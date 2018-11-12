3192d5c33dd87cf800bebabf1c17b13a926c18a6
HodorandLexi <jmj89421844@163.com>
2018-11-12 15:22:05 +0800

    初始

---
+++ scripts/test_contact.py
@@ -0,0 +1,22 @@
+import time
+import pytest
+
+from base.base_driver import init_driver
+from page.page import Page
+from base.base_analyze import analyze_file
+
+
+class TestContact:
+
+    def test_c1(self):
+        assert 1
+
+    def test_c2(self):
+        assert 1
+		
+	def test_c3(self):
+        assert 1
+
+	def test_c3(self):
+        assert 0
+
